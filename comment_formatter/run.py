d295627725b34e62520c6ba01093ef1f00fead65
AlexPadron <alexp@kensho.com>
2019-10-17 08:58:29 -0400

    Add check command

---
+++ comment_formatter/run.py
@@ -0,0 +1,36 @@
+from glob import glob
+import os
+
+import click
+
+from .formatter import rewrite_comments, rewrite_file
+
+
+@click.command()
+@click.argument("path", type=click.Path(exists=True))
+@click.option("--line-length", type=int, default=100)
+@click.option("--check", is_flag=True)
+def run(path: str, line_length: int, check: bool) -> None:
+    """CLI for comment formatting"""
+    if os.path.isdir(path):
+        python_files = glob(os.path.join(path, "**/*.py"), recursive=True)
+    else:
+        python_files = [path]
+
+    files_to_reformat = []
+
+    for file_name in python_files:
+        if rewrite_file(file_name, check):
+            pass
+        else:
+            files_to_reformat.append(file_name)
+
+    if len(files_to_reformat) == 0:
+        exit(0)
+    else:
+        click.echo("Files {} would be reformatted".format(files_to_reformat))
+        exit(1)
+
+
+if __name__ == "__main__":
+    run()
